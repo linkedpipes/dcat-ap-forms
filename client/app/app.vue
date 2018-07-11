95579dfea07da1b545c15c8ee5f52417295f77fb
Petr Škoda <skodapetr@gmail.com>
2018-07-11 09:29:36 +0200

    Remove footer

--- client/app/app.vue
+++ client/app/app.vue
@@ -4,8 +4,6 @@
             <v-toolbar-title>{{$labels.get("app_header")}}</v-toolbar-title>
         </v-toolbar>
         <router-view/>
-        <v-footer>
-        </v-footer>
     </v-app>
 </template>
 

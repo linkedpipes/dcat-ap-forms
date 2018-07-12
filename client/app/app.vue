181b14ea7240226bc839528f0c443923f2660a80
Petr Škoda <skodapetr@gmail.com>
2018-07-12 21:24:16 +0200

    Set page title typography class

--- client/app/app.vue
+++ client/app/app.vue
@@ -2,7 +2,7 @@
     <v-app>
         <v-toolbar>
             <v-toolbar-title>
-                <h1>{{$labels.get("app_header")}}</h1>
+                <h1 class="headline">{{$labels.get("app_header")}}</h1>
             </v-toolbar-title>
         </v-toolbar>
         <router-view/>

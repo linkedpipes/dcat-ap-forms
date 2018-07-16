870502f7c022aa470b3e53755b87d1b21eb874ad
Petr Škoda <skodapetr@gmail.com>
2018-07-16 17:52:15 +0200

    Remove lorem ipsum from help

--- client/app-service/help.vue
+++ client/app-service/help.vue
@@ -2,17 +2,7 @@
     <v-bottom-sheet v-if="data.isOpen" v-model="data.isOpen" lazy>
         <v-card>
             <v-card-text style="padding-bottom: 0px">
-                <!-- {{$labels.get('help_' + data.name)}} -->
-                Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Sed
-                convallis magna eu sem. Cras elementum. Aenean fermentum risus
-                id tortor. Mauris tincidunt sem sed arcu. Pellentesque pretium
-                lectus id turpis.
-
-                Mauris tincidunt sem sed arcu. Phasellus enim erat, vestibulum
-                vel, aliquam a, posuere eu, velit. Mauris suscipit, ligula sit
-                amet pharetra semper, nibh ante cursus purus, vel sagittis velit
-                mauris vel metus. Sed elit dui, pellentesque a, faucibus vel,
-                interdum nec, diam.
+                 {{$labels.get('help_' + data.name)}}
             </v-card-text>
             <v-layout justify-end="true">
                 <v-btn v-on:click="data.isOpen = false">

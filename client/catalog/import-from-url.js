175eead0d2097e8a8ddee74b09fc8d4e465bbdb3
Petr Škoda <skodapetr@gmail.com>
2018-08-13 13:37:31 +0200

    Add missing file

---
+++ client/catalog/import-from-url.js
@@ -0,0 +1,57 @@
+import {getRemoteJson} from "@/app-service/http";
+import {
+    normalize,
+    getDefaultGraphData,
+    getByType,
+    getValue,
+    getValues,
+    getByIri,
+    getId
+} from "@/app-service/jsonld";
+import {
+    DCTERMS,
+    DCATAP,
+    FOAF,
+    VCARD,
+    SCHEMA,
+    PU,
+    SKOS,
+    NKOD
+} from "@/app-service/vocabulary";
+
+function update_url(url) {
+    if (DEREFERENCE_PROXY === "") {
+        return url;
+    } else {
+        return DEREFERENCE_PROXY.replace("{}", encodeURIComponent(url));
+    }
+}
+
+export function importCatalog(url) {
+    return getRemoteJsonLd(url).then((response) => {
+        const graphData = getDefaultGraphData(normalize(response.json));
+        const dataset = getByType(graphData, DCATAP.Dataset)[0];
+        if (dataset === undefined) {
+            throw {"error": "FETCH"};
+        }
+        return parseCatalog(dataset);
+    });
+}
+
+function getRemoteJsonLd(url) {
+    return getRemoteJson(update_url(url), "application/ld+json");
+}
+
+function parseCatalog(dataset) {
+    const catalogUrl = getValue(dataset, NKOD.lkod);
+    if (catalogUrl === undefined) {
+        throw {
+            "error": "INVALID_DATA",
+            "message": "missing_lkod_reference"
+        };
+    } else {
+        return {
+            "url": catalogUrl
+        }
+    }
+}

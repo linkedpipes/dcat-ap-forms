657165eedbad4bc64a8659f24ac706d95a94c51a
Petr Škoda <skodapetr@gmail.com>
2018-11-08 08:05:08 +0100

    Apply check style, update dependencies

---
+++ babel.config.js
@@ -0,0 +1,31 @@
+module.exports = (api) => {
+
+    api.cache.using(() => process.env.NODE_ENV);
+
+    const presets = [
+        [
+            "@babel/preset-env",
+            {
+                "targets": {
+                    "ei": 11,
+                    "chrome": 41
+                }
+            }
+        ]
+    ];
+
+    const plugins = [];
+
+    const ignore = [];
+
+    if (api.env(["development"])) {
+        plugins.push("react-hot-loader/babel");
+        ignore.push("node_modules");
+    }
+
+    return {
+        "presets": presets,
+        "plugins": plugins,
+        "ignore": ignore
+    }
+};
---
+++ client/.eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "extends": [
+    "plugin:vue/recommended"
+  ],
+  "rules": {
+    "quotes": ["error", "double"],
+    "object-shorthand": ["error", "never"],
+    "quote-props": ["error", "always"],
+    "no-use-before-define": ["error", "nofunc"],
+    "prefer-destructuring": 0
+  }
+};

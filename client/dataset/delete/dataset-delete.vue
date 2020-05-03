408d836b49188172db3e5559497388b9b60f02e7
Petr Škoda <skodapetr@gmail.com>
2020-05-03 09:54:39 +0200

    Fix dataset loading
    #118

--- client/dataset/delete/dataset-delete.vue
+++ client/dataset/delete/dataset-delete.vue
@@ -1,8 +1,8 @@
 <template>
-  <v-content v-if="data.status === 'ready'">
-    <app-summary :dataset="data.dataset" />
+  <v-content v-if="status === 'ready'">
+    <app-summary :dataset="dataset" />
   </v-content>
-  <v-content v-else-if="data.status === 'loading'">
+  <v-content v-else-if="status === 'loading'">
     <!-- No content. -->
   </v-content>
   <v-content v-else>
@@ -23,29 +23,27 @@ export default {
     "app-summary": DatasetDeleteSummary,
   },
   "data": () => ({
-    "data": {
-      "status": "loading",
-      "dataset": undefined,
-      "error": undefined,
-    },
+    "status": "loading",
+    "dataset": undefined,
+    "error": undefined,
   }),
   "mounted": function () {
     setPageTitle(this.$t("delete_page_title"));
 
     const url = this.$route.query.url;
     if (url === undefined) {
-      this.data.status = "missing-dataset";
+      this.status = "missing-dataset";
       return;
     }
 
     const lang = this.$vuetify.lang.current;
-    importDatasetFromUrlWithProxy(url, lang).then((dataset) => {
-      this.data.dataset = dataset;
-      this.data.status = "ready";
+    importDatasetFromUrlWithProxy(url, lang).then((result) => {
+      this.dataset = result.dataset;
+      this.status = "ready";
     }).catch((error) => {
       console.error(error);
-      this.data.status = "error";
-      this.data.error = error;
+      this.status = "error";
+      this.error = error;
     });
   },
 };

4cf55fa921933b093621558f1c05b643d45266e5
Petr Škoda <skodapetr@gmail.com>
2026-06-27 17:00:58 +0200

    Fix tests
    Signed-off-by: Petr Škoda <skodapetr@gmail.com>

--- jest.config.js
+++ jest.config.js
@@ -8,6 +8,7 @@ module.exports = {
     "vue",
   ],
   "moduleNameMapper": {
+    "^vue$": "<rootDir>/node_modules/vue/dist/vue.runtime.common.prod.js",
     "^@/(.*)$": "<rootDir>/client/$1",
   },
   "transform": {
@@ -21,5 +22,10 @@ module.exports = {
     "client",
     "server",
   ],
-  "globals": {},
+  "globals" :{
+    "window": {}
+  },
+  "setupFiles": [
+    "<rootDir>/client/jest-setup.js",
+  ],
 };

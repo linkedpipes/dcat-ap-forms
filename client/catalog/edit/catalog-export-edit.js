632824c03bd74e56f616cf6b9fb7c1b07d420a58
Jakub Klímek <jakub@jakubklimek.com>
2026-07-28 11:57:01 +0200

    Změnit kontext pro soubory katalogu (#219)

--- client/catalog/edit/catalog-export-edit.js
+++ client/catalog/edit/catalog-export-edit.js
@@ -1,6 +1,6 @@
 const CONTEXT =
-  "https://ofn.gov.cz/dcat-ap-cz-rozhraní-katalogů-otevřených-dat/"
-  + "2024-05-28/kontexty/rozhraní-katalogů-otevřených-dat.jsonld";
+  "https://ofn.gov.cz/dcat-ap-cz-rozhraní-katalogů-dat/"
+  + "draft/katalog/kontext.jsonld";
 
 export function exportCatalogToJsonLd(catalog) {
   const output = {
